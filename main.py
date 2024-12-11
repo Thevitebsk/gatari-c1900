5dd2a3e6f60205854c62de5c5bced4832bea931e
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-11 23:18:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 inp="";instruct=[];reg={}
 def itergal(code:str):
  code=code.split();print(code)
- if code[0]=="REG":reg[code[1]]=int(code[2])
+ if code[0]=="REG":reg[code[1]]=code[2]
 def GAL():
  while True:
-  inp=input("C/GAL:")
+  inp=input("C/GAL:").upper()
   instruct.append(inp)
   if inp=="END":instruct.clear();reg.clear();break
   if inp=="RUN":
@@ -12,11 +12,11 @@ def GAL():
    while len(instruct)>0:itergal(instruct[0]);instruct.pop(0)
 def main():
  while True:
-  inp=input("C:")
+  inp=input("C:").upper()
   inpl=inp.split()
   if inpl[0]=="NEW":
    if len(inpl)>2:
-    with open(f'{" ".join(inpl[2:]).upper()}.{inpl[1].lower()}', 'w') as f:...
+    with open(f'{" ".join(inpl[2:]).upper()}.{inpl[1].lower()}', 'a') as f:...
    else:print("NOT ENOUGH DATA")
   elif inp=="END":break
   elif inp=="ASM":GAL()
