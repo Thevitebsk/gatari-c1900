e4d50df057dc2e67c3dffed853d40ef41130a7d4
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-11 22:07:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ def GAL():
   instruct=[]
   inp=input("C/GAL:")
   instruct.append(inp)
-  if inp=="EXIT":break
+  if inp=="EXIT":instruct.clear();break
 def main():
  while True:
   inp=input("C:")
   inpl=inp.split()
   if inpl[0]=="NEW":
    with open(f'{" ".join(inpl[2:])}.{inpl[1]}', 'w') as f:...
-  if inp=="ASM":GAL()
+  elif inp=="END":break
+  elif inp=="ASM":GAL()
 print(
 """
 ****************
