0afcf4520bb4f9a28e2858e01c0bb1b7d4401138
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-12 22:18:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-inp="";instruct=[];reg={};rc=0;l=[]
-def itergal(code:str):
+inp="";instruct=[];reg={}
+def itergal(code:str)->bool:
  "Interpreter for Gatari ASM Language"
  code=code.split()
  if code[0]=="REG":reg[code[1]]=code[2]
- else:print(f"\"{code[0]}\" IS NOT VALID GAL OPERATOR");rc=1;pass
+ else:print(f"\"{code[0]}\" IS NOT VALID GAL OPERATOR");return 1
 def GAL():
+ global l
  "Gtari ASM Language Interface"
  while True:
   inp=input("C/GAL:").upper()
   instruct.append(inp)
-  if inp=="END":l=instruct;instruct.clear();reg.clear();break
+  if inp=="END":instruct.clear();reg.clear();break
+  if inp=="LIST":instruct.pop();print("\n".join(instruct))
   if inp=="RUN":
    instruct.pop()
-   while len(instruct)>0:itergal(instruct[0]);instruct.pop(0)
+   while instruct:
+    if itergal(instruct[0]):break
+    instruct.pop(0)
+   print(len(instruct),"bytes")
 def MAIN():
  "Main interface"
  while True:
